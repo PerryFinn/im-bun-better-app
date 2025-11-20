63c8e0b9147ad2f7654e6cd3da5632da50eef674
陈鹏飞 <chenpengfei@cvte.com>
2025-11-20 15:05:21 +0800

    change default theme to system

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -44,8 +44,9 @@ function RootComponent() {
       <HeadContent />
       <ThemeProvider
         attribute="class"
-        defaultTheme="dark"
+        defaultTheme="system"
         disableTransitionOnChange
+        enableSystem
         storageKey="vite-ui-theme"
       >
         <div className="grid h-svh grid-rows-[auto_1fr]">
